a18087fcb9f6668c5f4551e6e7b10e52b10774fa
CDS-admin1 <csouthon44@gmail.com>
2023-03-11 15:06:58 +0000

    Added working navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -8,7 +8,41 @@ import './styles/Navbar.css';
 
 function NavigationBar() {
     return (
-       
+       <ul className="nav nav-tabs linkFont justify-content-center">
+          <li className="nav-item">
+            <NavLink
+              to="/"
+              end
+              className={({ isActive}) =>
+                isActive ? 'nav-link active' : 'nav-link'
+            }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="projects"
+              end
+              className={({ isActive}) =>
+                isActive ? 'nav-link active' : 'nav-link'
+            }
+            >
+              Projects
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="contact"
+              end
+              className={({ isActive}) =>
+                isActive ? 'nav-link active' : 'nav-link'
+            }
+            >
+              Contact
+            </NavLink>
+          </li>
+       </ul>
     )
 };
 
